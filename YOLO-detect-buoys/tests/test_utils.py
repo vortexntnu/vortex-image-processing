9190afa48684ce9b36719e2225c7b6aada1f3c37
Yauhen Yavorski <yauhen.yavorski@gmail.com>
2024-03-17 17:40:28 +0100

    💡 Add test case for get_device function

--- YOLO-detect-buoys/tests/test_utils.py
+++ YOLO-detect-buoys/tests/test_utils.py
@@ -9,6 +9,8 @@ from utils import get_device
 def test_get_device() -> None:
     """
     Test the get_device function.
+    This makes sure that if the function is changed, the tests verifies that
+    the function returns the expected device.
     """
     expected_device = (
         device(device="cuda") if cuda.is_available() else device(device="cpu")
--- YOLO-detect-buoys/utils/get_device.py
+++ YOLO-detect-buoys/utils/get_device.py
@@ -8,6 +8,7 @@ from torch import device
 def get_device() -> device:
     """
     Get the device to use for training and inference.
+    https://stackoverflow.com/questions/48152674/how-do-i-check-if-pytorch-is-using-the-gpu
 
     Returns:
         torch.device: The device to use for training and inference.

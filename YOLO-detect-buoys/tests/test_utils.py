722797272b08154e73dfad97fedb0e94df5311a0
Yauhen Yavorski <yauhen.yavorski@gmail.com>
2024-03-19 17:23:09 +0100

    ✅ Add test for get_data function

--- YOLO-detect-buoys/tests/test_utils.py
+++ YOLO-detect-buoys/tests/test_utils.py
@@ -2,8 +2,11 @@
 Module for the utility functions.
 """
 
+from unittest.mock import Mock, patch
+
+import pytest
 from torch import cuda, device
-from utils import get_device
+from utils import get_data, get_device
 
 
 def test_get_device() -> None:
@@ -16,3 +19,29 @@ def test_get_device() -> None:
         device(device="cuda") if cuda.is_available() else device(device="cpu")
     )
     assert get_device() == expected_device
+
+
+def test_get_data() -> None:
+    """
+    Test the get_data function.
+    This makes sure that if the function is changed, the tests verifies that
+    the function returns the expected dataset location.
+    """
+    with patch("os.getenv") as mock_getenv:
+        mock_getenv.return_value = "ROBOFLOW_API_KEY"
+        with patch("roboflow.Roboflow") as mock_roboflow:
+            mock_roboflow.return_value = Mock()
+            mock_roboflow.return_value.workspace.return_value = Mock()
+            mock_roboflow.return_value.workspace.return_value.project.return_value = (
+                Mock()
+            )
+            mock_roboflow.return_value.workspace.return_value.project.return_value.version.return_value = (
+                Mock()
+            )
+            mock_roboflow.return_value.workspace.return_value.project.return_value.version.return_value.download.return_value = (
+                Mock()
+            )
+            mock_roboflow.return_value.workspace.return_value.project.return_value.version.return_value.download.return_value.location = (
+                "https://example.com"
+            )
+            assert get_data("project_id", 1) == "https://example.com"

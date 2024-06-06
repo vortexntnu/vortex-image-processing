a1480e0bd2abd1c03c82f5b895f11e8c01f810a8
Yauhen Yavorski <yauhen.yavorski@gmail.com>
2024-06-06 15:40:27 +0200

    ✨ feat: Push the model to the hub

--- YOLO-detect-buoys/__main__.py
+++ YOLO-detect-buoys/__main__.py
@@ -2,12 +2,18 @@
 The main entry point for YOLO detecter buoys.
 """
 
-from os import getcwd, path
+from os import getcwd, getenv, path
 
+from dotenv import load_dotenv
+from huggingface_hub import HfApi, Repository
 from ultralytics import YOLO
 from utils import get_data, get_device, process_video
 
 if "__main__" == __name__:
+
+    load_dotenv()
+
+    HfApi().whoami(token=getenv("HF_TOKEN"))
 
     device = get_device()
     model = YOLO("yolov8n.pt")
@@ -16,12 +22,18 @@ if "__main__" == __name__:
     dataset = get_data()
 
     result = model.train(
-            data=dataset.location + "/data.yaml", epochs=50, imgsz=640, device=device
+        data=dataset.location + "/data.yaml", epochs=50, imgsz=640, device=device
     )
 
     model.val()
 
-    process_video("https://youtu.be/4WGpIOwkLA4?feature=shared", model)
+    path = model.export(format="onnx")  # export to onnx
+
+    # Initialize the repository
+    repo = Repository(local_dir=path, clone_from=getenv("HG_REPO_ID"))
+
+    # Commit and push your changes
+    repo.push_to_hub(commit_message="push the model to the hub")
 
 # References:
 # https://docs.ultralytics.com/quickstart/#install-ultralytics

3f545ba701fcfb3ec3e5ee609fac1c20da2d9102
Yauhen Yavorski <yauhen.yavorski@gmail.com>
2024-08-22 18:48:08 +0200

    ✨ feat: Update YOLO model training parameters

--- YOLO-detect-buoys/__main__.py
+++ YOLO-detect-buoys/__main__.py
@@ -14,18 +14,24 @@ if "__main__" == __name__:
     load_dotenv()
 
     device = get_device()
+    print("device", device)
     model = YOLO("yolov8n.pt")
 
     file_path = path.abspath(getcwd())
     dataset = get_data()
 
     result = model.train(
-        data=dataset.location + "/data.yaml", epochs=50, imgsz=640, device=device
+        data=dataset.location + "/data.yaml",
+        epochs=100,
+        imgsz=640,
+        device=device,
+        batch=4,
+        cache=False,
     )
 
     model.val()
 
-    path = model.export()  # export to onnx
+    path = model.export(format="onnx")  # export to onnx
 
     print("Model exported to: " + path)
     print(path)

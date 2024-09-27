5487bdf0973353cd82f479e0257510f563b78c70
Yauhen Yavorski <yauheny@stud.ntnu.no>
2024-09-27 13:47:31 +0200

    ✨  Sphinx documentation configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../YOLO-detect_buoys"))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,22 +10,28 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'vortex image build'
-copyright = '2024, Yauhen Yavorski'
-author = 'Yauhen Yavorski'
+project = "vortex image build"
+copyright = "2024, Yauhen Yavorski"
+author = "Yauhen Yavorski"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
+
+extensions = [
+    "sphinx.ext.autodoc",  # To automatically document docstrings
+    "sphinx.ext.napoleon",  # To support Google-style and NumPy-style docstrings
+    "sphinx.ext.viewcode",  # To link to the source code
+    "sphinx.ext.autosummary",  # To create summaries of your code
+]
